2754a42801e04b6596a194bd2c99282eaae1ca7f
Benjamin Adubofour <benjamin.adubofourjunior@student.manchester.ac.uk>
2025-12-25 16:37:22 +0000

    removed unused import

--- src/main/java/com/pokergame/security/JwtService.java
+++ src/main/java/com/pokergame/security/JwtService.java
@@ -1,14 +1,12 @@
 package com.pokergame.security;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.util.Base64;
 import java.util.Date;
 
 /**
